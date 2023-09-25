dd28e91e4af6ea5067066008e0fcf9e3d4067b2f
macbook <yakovenko06122002@icloud.com>
2023-09-25 22:52:40 +0300

    Урок 3, домашка

--- LearnWords-Nikita/ViewController.swift
+++ LearnWords-Nikita/ViewController.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class Test {
+    var text: String = " "
+}
+
+class ViewController: UIViewController {
+    
+    var click = 0
+
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let value = Test()
+        
+        value.text = "Новое значение"
+        print(value.text) //1
+        
+        
+//        let text = "Новое значение"
+        view.backgroundColor = .yellow
+        label.backgroundColor = .clear
+        
+        
+        setup(string: value)
+        print(value.text)
+
+    }
+    
+    func setup(string test: Test) {
+        test.text = "agdagdgagdaga"
+        label.text = test.text
     }
 
-
+    @IBAction func tapButtonAction(_ sender: UIButton) {
+        
+        startAction()
+    }
+    
+    
+    func startAction() {
+        let text = "Новое значение"
+        click += 1
+        label.text = text + String(click)
+    }
 }
 
